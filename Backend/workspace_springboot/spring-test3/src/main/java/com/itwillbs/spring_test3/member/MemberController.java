bb1b26fb55347fcdfa03cea62cf3455bac6319f7
spy <spycode92@gmail.com>
2025-10-24 16:16:03 +0900

    251024 6

--- Backend/workspace_springboot/spring-test3/src/main/java/com/itwillbs/spring_test3/member/MemberController.java
+++ Backend/workspace_springboot/spring-test3/src/main/java/com/itwillbs/spring_test3/member/MemberController.java
@@ -1,27 +1,144 @@
 package com.itwillbs.spring_test3.member;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+	/*
+	 * A 클래스가 B 클래스에 의존적일 때 B 클래스에 대한 인스턴스를 직접 생성하지 않고,
+	 * 의존성 자동 주입(DI = Dependency Injection) 기능을 통해 인스턴스를 전달받아 사용할 수 있다!
+	 * 
+	 * [ 의존성 주입을 받는 방법 3가지(+ 1가지) ]
+	 * 0) 매핑 메서드 파라미터로 주입받을 클래스 타입 매개변수를 선언하는 방법
+	 *    => 매핑 메서드가 호출될 때마다 해당 클래스 인스턴스가 생성되어 주입됨(매번 새로운 객체)
+	 *    => 단, 이 클래스 타입의 매개변수는 메서드 내에서만 접근 가능하며
+	 *       다른 메서드에서는 또 다른 인스턴스를 통해 주입받아 사용해야하므로 매개변수 선언 또 필요
+	 *    => 해당 클래스를 평소처럼 정의하면 됨(별다른 추가 작업(어노테이션 설정) 불필요)
+	 * -------------------------------------------------------------------------
+	 * 만약, 멤버변수(필드) 형태로 주입받을 클래스 타입 변수를 선언하여 메서드마다 공유하는 경우
+	 * => 해당 클래스(주입받을 클래스) 정의 시 @Component, @Service 등의 어노테이션을 통해 스프링 빈으로 등록 필수!
+	 * => 어노테이션 미지정 시 자동 주입 불가능한 상태로 오류 발생함
+	 * 1) 생성자를 통해 의존성 객체를 주입받는 방법
+	 *    => 빈으로 등록하지 않았을 경우 오류 메세지 : Parameter 0 of constructor in com.itwillbs.spring_test3.member.MemberController required a bean of type 'com.itwillbs.spring_test3.member.MemberService' that could not be found.)
+	 * 2) Setter 메서드를 통해 의존성 객체를 주입받는 방법(Setter 메서드에 @Autowired 어노테이션 필수)
+	 *    => 필드에 @Autowired 어노테이션 생략 시 오류 메세지 : java.lang.NullPointerException: Cannot invoke "com.itwillbs.spring_test3.member.MemberService.getMemberList()" because "this.memberService" is null
+	 * 3) 멤버변수 선언 시 의존성 객체를 주입받는 방법(멤버변수에 @Autowired 어노테이션 필수)
+	 * 4) final 필드 형태로 선언한 후 생성자를 통해 객체 주입
+	 *    => 파라미터 생성자를 직접 정의하거나 Lombok 의 @RequiredArgsConstructor 어노테이션을 지정하여 주입
+	 */
 	
+	// 1) 생성자를 통해 의존성 객체를 주입받는 방법
+//	private MemberService memberService;
+//	public MemberController(MemberService memberService) {
+//		System.out.println("MemberController 생성자 호출됨!");
+//		this.memberService = memberService;
+//	}
+	// ---------------------
+	// 2) Setter 메서드를 통해 의존성 객체를 주입받는 방법(Setter 메서드에 @Autowired 어노테이션 필수)
+//	private MemberService memberService;
+//	
+//	@Autowired
+//	public void setMemberService(MemberService memberService) {
+//		this.memberService = memberService;
+//	}
+	// ---------------------
+	// 3) 멤버변수 선언 시 의존성 객체를 주입받는 방법(멤버변수에 @Autowired 어노테이션 필수)
+//	@Autowired
+//	private MemberService memberService;
+	// ---------------------
+	// 4) final 필드 형태로 선언한 후 생성자를 통해 객체 주입
+	// => 첫번째방법) 생성자를 직접 정의해서 MemberService 타입 파라미터를 선언
+	// => 두번째방법) 생성자를 직접 정의하는 대신 Lombok 의 @RequiredArgsConstructor 어노테이션을 지정
+	private final MemberService memberService;
+//	public MemberController(MemberService memberService) {
+//		System.out.println("MemberController 생성자 호출됨!");
+//		this.memberService = memberService;
+//	}
+	// => 주의! @RequiredArgsConstructor 어노테이션 사용 시 생성자를 직접 정의하면 안됨! (= 생성자 중복됨)
+	// ====================================================================================
+
 	@GetMapping("/memberList")
+//	public List<Member> showMemberList(MemberService memberService) { // 메서드 호출 시점에 스프링 빈으로 등록된 MemberService 인스턴스 주입됨
 	public List<Member> showMemberList() {
-		MemberService memberService = new MemberService();
-		
+		// 개발자가 객체가 필요한 시점에 직접 인스턴스를 생성하여 사용하는 경우
+//		MemberService memberService = new MemberService();
+//		memberService.test();
+//		return null;
+		// ==========================================================================
+		// 스프링 DI 에 의해 MemberService 객체를 자동 주입받은 상태
+		// ---------------------------------------------------------
 		// MemberService - getMemberList() 메서드 호출하여 비즈니스 로직 처리 요청
 		// => 파라미터 : 없음    리턴타입 : List<Member>(memberList)
 		List<Member> memberList = memberService.getMemberList();
 		
-		// 스프링부트는 Jackson 라이브러리가 기본으로 포함되어 동작하기 때문에
-		// 매핑 메서드에서 객체 리턴 시 자동으로 JSON 형식으로 변환되어 응답 데이터로 전송함
+		// @ResponseBody 또는 @RestController 가 적용된 메서드에서 특정 객체를 리턴할 경우
+		// 스프링부트에 Jackson 라이브러리(spring-boot-starter-web 라이브러리 추가 시 함께 로딩됨)가 기본 JSON 처리 라이브러리로 동작하므로
+		// 매핑 메서드에서 객체 리턴 시 자동으로 JSON 형식의 문자열로 변환되어 응답 데이터로 전송함
+		// => 리턴할 객체를 대상으로 Jackson 라이브러리의 메세지 컨버터에 의해 JSON 문자열 형태로 직렬화를 수행한다!
 		return memberList; // List<Member> 타입 객체가 JSON 형식 문자열로 자동 변환되어 리턴
 	}
 	
 	
+	// /members/memberInfo/xxx 에 대한 요청 매핑
+	// => xxx 부분을 경로 변수 Long 타입 id 로 바인딩
+	@GetMapping("/memberInfo/{id}")
+	public Member showMemberInfo(@PathVariable("id") Long id) {
+//		System.out.println("id : " + id);
+		
+		// MemberService - getMember() 메서드 호출하여 비즈니스 로직 처리 요청
+		// => 파라미터 : id   리턴타입 : Member
+		Member member = memberService.getMember(id);
+		
+		return member;
+	}
+	
+	// 이름으로 조회 요청
+	@GetMapping("/memberInfoByName")
+	public Member memberInfoByName(@RequestParam("name") String name) {
+		System.out.println("name : " + name);
+		
+		// MemberService - getMemberByName() 메서드 호출하여 비즈니스 로직 처리 요청
+		// => 파라미터 : name   리턴타입 : Member
+		Member member = memberService.getMemberByName(name);
+		
+		return member;
+	}
+	
+	// 이름으로 조회 요청
+	@GetMapping("/searchMember")
+	public Member searchMember(@RequestParam Map<String, String> params) {
+		System.out.println("id : " + params.get("id"));
+		System.out.println("name : " + params.get("name"));
+		
+		// MemberService - searchMember() 메서드 호출하여 비즈니스 로직 처리 요청
+		// => 파라미터 : Map 객체   리턴타입 : Member
+		Member member = memberService.searchMember(params);
+		
+		return member;
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
--- Backend/workspace_springboot/spring-test3/src/main/java/com/itwillbs/spring_test3/member/MemberService.java
+++ Backend/workspace_springboot/spring-test3/src/main/java/com/itwillbs/spring_test3/member/MemberService.java
@@ -1,17 +1,118 @@
 package com.itwillbs.spring_test3.member;
 
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Component // 스프링 빈으로 등록하는 공통 어노테이션
+@Service // 스프링 빈으로 등록하는 어노테이션. 서비스 역할(= 비즈니스 로직 처리)을 수행하는 용도라는 의미의 어노테이션(@Component 어노테이션을 포함함)
 public class MemberService {
+	// 스프링 빈으로 등록된 MemberRepository 객체 자동 주입 설정
 	@Autowired
 	private MemberRepository memberRepository;
 	
+	// DI 확인을 위한 임시 메서드
+//	public void test() {
+//		System.out.println("MemberService - test()");
+//	}
+	
 	public List<Member> getMemberList() {
+		System.out.println("MemberService - getMemberList()");
+		
+		// 전체 member 테이블(정확히는 Member 엔티티의 목록)을 조회하는 작업 요청
+		// => JPA 에서 데이터베이스 작업을 실질적으로 처리해주는 Repository 의 특정 메서드를 호출하여 데이터 조회 요청 가능
+		// => 테이블의 전체 레코드를 조회하는 구문(SELECT * FROM xxx) 을 수행하는 메서드는
+		//    별도의 쿼리 메서드 정의없이 기본으로 제공되는 findAll() 메서드 호출하여 목록 조회 및 데이터 리턴 가능 
+		// => findAll() 메서드 리턴타입이 기본적으로 List<T> 타입인데
+		//    MemberRepository 인터페이스 정의 시 extends JpaRepository<Member, Long> 형태로 상속을 표현했을 때
+		//    제네릭타입 첫번째로 지정된 엔티티 클래스 타입 Member 가 T 부분에 해당하는 타입으로 적용됨
+		//    따라서, 해당 엔티티와 연결된 테이블을 조회하여 List<T> 타입 대신 엔티티에 대한 타입인 List<Member> 타입으로 변경되어 객체가 리턴됨 
 		return memberRepository.findAll();
 	}
 
+	public Member getMember(Long id) {
+		System.out.println("MemberService - getMember()");
+		
+		// member 테이블 내에서 id 값이 일치하는 1개의 레코드 조회(SELECT * FROM member WHERE id = ?)
+		// => JpaRepository 를 통해 findByXXX() 메서드가 제공됨(특정 대상(XXX)을 기준으로 레코드를 검색하는 용도의 메서드)
+		// => @Id 가 붙은 필드(Long id)를 기준으로 레코드를 검색하기 위해 findById() 메서드 호출
+		//    (주의! id 컬럼명이 아닌 @Id 어노테이션이 붙은 필드에 연결된 컬럼(PK 컬럼)을 뜻함)
+		// => findById() 메서드 리턴값에 대해 get() 메서드 호출 시 조회된 엔티티 1개가 리턴됨
+//		return memberRepository.findById(id).get(); // Optional<Member> 내에서 Member 엔티티를 꺼내서 리턴
+		// => findById() 메서드 리턴타입은 Optional<T> 타입이며, 현재 T 타입이 Member 엔티티 타입이므로 Optional<Member> 타입 리턴됨
+		// => 주의! 조회 결과가 없을 경우 조회되는 엔티티도 비어있게 되며, 이 때 Optional<T> 객체의 get() 메서드 호출 시 예외 발생함
+		//    (java.util.NoSuchElementException: No value present)
+		// => 따라서, 조회 대상이 없을 경우에 대한 처리를 Optional 객체를 통해 추가적으로 수행하는 것이 안전함
+		// --------------------------------------------------
+		// [ 첫번째 방법 ]
+		// Optional<Member> 타입을 리턴받아서 조회 결과가 있을 경우에 조회 결과를 담은 Member 엔티티를 리턴하고
+		// 조회 결과가 없을 경우 Member 엔티티를 리턴하지 않거나 빈 엔티티를 리턴하도록 처리하는 방법 예시
+//		Optional<Member> optionalMember = memberRepository.findById(id);
+//		System.out.println("optionalMember : " + optionalMember);
+		// => 조회 결과가 없을 경우 optionalMember : Optional.empty
+		// => 조회 결과가 있을 경우 optionalMember : Optional[Member(id=2, name=이순신)]
+		
+		// 조회 결과가 없을 경우를 판별하여 추가적인 작업 수행 가능
+		// => Optional 객체의 isEmpty() 메서드를 사용하여 데이터가 없음을 판별 가능 <-> isPresent() 메서드로 데이터가 있음을 판별 가능
+//		if(optionalMember.isEmpty()) {
+//			System.out.println("조회 결과 없음!");
+//			
+//			throw new NoSuchElementException("존재하지 않는 회원입니다!");
+//			// 또는 
+//			return new Member(); // 빈 객체를 생성하여 리턴하는 등의 추가 작업 가능
+//		}
+//		
+//		System.out.println("조회 결과 있음!");
+//		return optionalMember.get();
+		// --------------------------------------------------
+		// [ 두번째 방법 ]
+		// Optional 객체의 orElse() 또는 orElseThrow() 메서드 활용하여 조회 결과가 없을 경우까지 대비하는 작업을 압축된 코드로 수행
+		// 1) findByXXX() 메서드 뒤에 orElseThrow() 메서드를 연쇄적으로 호출하여 메서드 파라미터로
+		//    조회 결과가 없을 때 실행할 예외 처리 클래스의 인스턴스를 화살표 함수 형태로 지정
+		//    => 조회 결과가 있으면 get() 메서드가 자동으로 호출되어 조회된 엔티티가 리턴되고
+		//       아니면, 지정한 예외 인스턴스가 실행되어 예외 메세지가 전달됨(별도의 예외 처리 클래스로 처리 가능)
+//		return memberRepository.findById(id)
+//								.orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다!"));
+		
+		// 2) orElse() 메서드 사용 시 예외를 발생시키는 대신 새로운 엔티티(빈 엔티티 등)를 생성하여 리턴
+		//    => 즉, 예외가 발생하지 않고 빈 값이 저장된 엔티티를 외부로 전달하여 안전하게 처리 가능
+		return memberRepository.findById(id)
+								.orElse(new Member());
+	}
+
+	// 이름으로 Member 엔티티 검색 요청
+	public Member getMemberByName(String name) {
+		// MemberRepository - findByXXX() 메서드가 제공되므로 findByName() 메서드 호출하면 이름으로 조회 가능
+		// => findById() 외의 나머지 컬럼(필드)을 조건으로 갖는 메서드는 자동으로 생성되어 제공되지 않으므로 추가로 정의해야함 
+		return memberRepository.findByName(name)
+				.orElseThrow(() -> new NoSuchElementException("해당 이름의 회원이 존재하지 않습니다!"));
+	}
+
+	// 아이디와 이름으로 Member 엔티티 검색 요청
+	public Member searchMember(Map<String, String> params) {
+		// MemberRepository - findByIdAndName() 메서드 추가 정의 필요
+		// 주의! Map<String, String> 타입일 때 id 파라미터는 Long 타입으로 전달되어야 하므로 문자열 -> Long 변환 필수!
+		return memberRepository.findByIdAndName(Long.parseLong(params.get("id")), params.get("name"))
+				.orElseThrow(() -> new NoSuchElementException("해당 번호와 이름이 일치하는 회원이 존재하지 않습니다!"));
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
